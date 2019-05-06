dbc2f37d0c01ad34b3d20fd38fb173cead4d73f6
TripleBlood <slava.ligai98@gmail.com>
2019-05-06 13:46:51 +0300

    bf

---
+++ src/main/java/ligai/controllers/CourseController.java
@@ -0,0 +1,81 @@
+package ligai.controllers;
+
+import ligai.enums.ProgressStatus;
+import ligai.enums.QuestionType;
+import ligai.models.*;
+import ligai.repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/android")
+public class CourseController {
+    private static final String template = "Hello, %s!";
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private CoursesRepository coursesRepository;
+
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Autowired
+    private ThemeRepository themeRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private QSelectOutOfFourRepository qSelectOutOfFourRepository;
+
+    @Autowired
+    private LessonProgressRepository lessonProgressRepository;
+
+    @Autowired
+    UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @GetMapping("/greeting")
+    public QSelectOutOfFour greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        long id = 1;
+        return qSelectOutOfFourRepository.findFirstById(id).orElse(null);
+    }
+
+    @GetMapping("/greeting/save")
+    public QSelectOutOfFour saveRandom() {
+        QSelectOutOfFour qSelectOutOfFour = QSelectOutOfFour.qSelectOutOfFourBuilder()
+                .task("Simple Task")
+                .lesson(null)
+                .questionType(QuestionType.ONE_OUT_OF_FOUR)
+                .answerString("1")
+                .variant1("FOX")
+                .variant2("DOG")
+                .variant3("CAT")
+                .variant4("DUCK")
+                .build();
+
+        qSelectOutOfFourRepository.save(qSelectOutOfFour);
+        return qSelectOutOfFour;
+    }
+
+
+    @GetMapping("/andLogin")
+    public User authUserByCredentials(@RequestParam(value = "login") String login,
+                                      @RequestParam(value = "pass") String pass) {
+        Optional<User> result = usersRepository.findFirstByLogin(login);
+
+        if (result.isPresent()) {
+            if (passwordEncoder.matches(pass, result.get().getPass())) {
+                return result.get();
+            }
+        }
+        throw new IllegalArgumentException("Login or password incorrect");
+    }
+}
